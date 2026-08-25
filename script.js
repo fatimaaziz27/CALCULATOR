448908a0ad16e691f22eb903f92e4023f7132c93
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-25 23:01:05 +0500

    fixio

--- script.js
+++ script.js
@@ -17,23 +17,18 @@ buttons.forEach(button => {
             try {
                 display.value = eval(display.value);
             }
-
             catch {
                 display.value = "Error";
             }
-
         }
 
         else {
-
             if (display.value === "0") {
                 display.value = value;
             }
-
             else {
                 display.value += value;
             }
-
         }
 
     });
