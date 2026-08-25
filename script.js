18e7e926867f73714b547c8b87666541bfd13638
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-25 23:01:33 +0500

    fixi

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ buttons.forEach(button => {
         if (value === "AC") {
             display.value = "";
         }
-
         else if (value === "DEL") {
             display.value = display.value.slice(0, -1);
         }
-
         else if (value === "=") {
             try {
                 display.value = eval(display.value);
@@ -30,4 +28,5 @@ buttons.forEach(button => {
             }
         }
     });
+    
 });
