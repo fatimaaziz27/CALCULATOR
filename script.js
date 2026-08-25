e7ae31dbc8c5522c6fdb5f0cf3ccb6ada9e9613d
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-25 22:47:28 +0500

    fix

---
+++ script.js
@@ -0,0 +1,48 @@
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        let value = button.innerText;
+
+        if (value === "AC") {
+
+            display.value = "";
+
+        }
+
+        else if (value === "DEL") {
+
+            display.value = display.value.slice(0, -1);
+
+        }
+
+        else if (value === "=") {
+
+            try {
+                display.value = eval(display.value);
+            }
+
+            catch {
+                display.value = "Error";
+            }
+
+        }
+
+        else {
+
+            if (display.value === "0") {
+                display.value = value;
+            }
+
+            else {
+                display.value += value;
+            }
+
+        }
+
+    });
+
+});
