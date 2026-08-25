3a5de4e7470048c07c6f043af9f7c1f06a69a581
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-25 23:00:49 +0500

    fixi

--- script.js
+++ script.js
@@ -2,25 +2,18 @@ let display = document.getElementById("display");
 let buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
-
     button.addEventListener("click", () => {
-
         let value = button.innerText;
 
         if (value === "AC") {
-
             display.value = "";
-
         }
 
         else if (value === "DEL") {
-
             display.value = display.value.slice(0, -1);
-
         }
 
         else if (value === "=") {
-
             try {
                 display.value = eval(display.value);
             }
